83dd6446ca0132b9cb34eb19a5722d9d81887ea9
Bojan Bogdanovic <bojan.bogdanovic@adera.no>
2024-06-08 18:32:37 +0200

    add creating receipt

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import https from "https"
 import path from "path";
 import admin from "firebase-admin"
 import cors from "cors"
+import nodemailer from "nodemailer";
 
 // import serviceAccount from "path/to/key.json"
 const app = express()
@@ -179,6 +180,104 @@ app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PATCH', 'PUT']
 }))
 // });
+app.post('/createReceipt', (req, res) => {
+    var products = req.body;
+    var totalPrice = 0;
+    products.forEach(prd => totalPrice += prd.price * prd.quantity)
+    console.log(products)
+
+    var li = ""
+    var table = ""
+    table += `<table  style="          font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%> 
+    <tr style=":nth-child(even) { background-color: #dddddd;}">
+    <th style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">Proizvod</th>
+    <th style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">Kolicina</th>
+    <th style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">Cena</th>
+    <th style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">Ukupno</th>
+</tr>
+    
+    `
+
+
+
+    products.forEach(product => {
+
+        table += `
+     <tr>
+     <td style=" border: 1px solid #dddddd;
+     text-align: left;
+     padding: 8px;">${product.name}</td>
+     <td style=" border: 1px solid #dddddd;
+     text-align: left;
+     padding: 8px;">${product.quantity}</td>
+     <td style=" border: 1px solid #dddddd;
+     text-align: left;
+     padding: 8px;">${product.price} din</td>
+     <td style=" border: 1px solid #dddddd;
+     text-align: left;
+     padding: 8px;">${product.price * product.quantity} din</td>
+ </tr>`
+    })
+    table += `
+    <tr>
+    <td style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">Total</td>
+
+  
+    <td style=" border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;">${totalPrice} din</td>
+</tr>`
+    table += '</table>'
+
+    var html = `<div style="width: 100%;">
+    <div style="width:300px;  position: relative; left: 50%; transform: translateX(-50%); text-align: center;  box-shadow: rr ">
+    <h1 style="margin-bottom: 40px;">BOGI SHOP</h1>
+     ${table}
+     <p style="text-align: left;    width: 300px;">Hvala na kupovini i ukazanom poverenju...</p>
+    </div>
+    
+    </div>
+    `
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+
+        // Use `true` for port 465, `false` for all other ports
+        auth: {
+            user: "bojanb0794@gmail.com",
+            pass: "hixrjsyerfvhwkif",
+        },
+    });
+
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: '"Bogi shop <bojanb0794@gmail.com>', // sender
+            to: "bojanb0794@gmail.com", // list of receivers
+            subject: "Porudzbenica Bogi Shop", // Subject line
+
+            html: html, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+    }
+
+    main().catch(console.error);
+})
 
 
 app.get('/', (req, res) => {
@@ -208,6 +307,22 @@ app.post('/login', async (req, res) => {
 
 
 })
+
+app.post('/register', async (req, res) => {
+    let user = users.find(user => user.email == req.body.email)
+
+
+    try {
+        if (user) throw new Error('User not exists');
+        users.push({ email: req.body.email, password: req.body.password });
+        res.json(200)
+    } catch (error) {
+        console.log("catch triggereddd")
+        res.status(404).json({ success: false, error: error.message });
+    }
+
+
+})
 app.put('/filterProducts', (req, res) => {
     const prodType = req.body.type.toLowerCase();
     const filteredProducts = prodType == "all" ? products : products.filter(prod => prod.type == prodType);
@@ -230,21 +345,7 @@ app.post('/comments', (req, res) => {
     res.json(products);
 
 })
-app.post('/register', async (req, res) => {
-    let user = users.find(user => user.email == req.body.email)
 
-
-    try {
-        if (user) throw new Error('User not exists');
-        users.push({ email: req.body.email, password: req.body.password });
-        res.json(200)
-    } catch (error) {
-        console.log("catch triggered")
-        res.status(404).json({ success: false, error: error.message });
-    }
-
-
-})
 
 
 // const httpsServer = https.createServer({
